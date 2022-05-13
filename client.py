271be448e0e739292bb9078ef998ef1d73c7d8e9
Daniele Pancottini <43572647+DanielePancottini@users.noreply.github.com>
2022-05-13 15:22:21 +0200

    Complete a basic PUT command implementation withour errors handling and without multithreading

--- client.py
+++ client.py
@@ -1,20 +1,38 @@
 
 import socket
-import json
 
 socketUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 serverAddress = ('localhost', 10000)
-message = 'list'
+command = 'put'
+filename = 'caro.txt'
 
 try:
     
-    print('sending %s' % message)
-    sent = socketUDP.sendto(message.encode(), serverAddress)
-    data, address = socketUDP.recvfrom(4096)
-    files = json.loads(data.decode())
+    print('sending put command')
+    sent = socketUDP.sendto(command.encode(), serverAddress)
     
-    print(files[0])
+    #wait for PUT ACK
+    ready, address = socketUDP.recvfrom(4096)
+    
+    #send filename
+    socketUDP.sendto(filename.encode(), address)
+    
+    #wait for filename ack
+    ready, address = socketUDP.recvfrom(4096)
+    
+    #start sending file data
+    f = open(filename, "rb")
+    
+    while True:
+        # read the bytes from the file
+        bytes_read = f.read(4096)
+        if not bytes_read:
+            # eof
+            socketUDP.sendto('FILE END'.encode(), address)
+            break
+        
+        socketUDP.sendto(bytes_read, address)
 
 except Exception as info:
     print(info)
--- server.py
+++ server.py
@@ -6,19 +6,58 @@ import json
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', 10000))
 
+    
+def saveFile(fileName, s):
+    
+    # open file into write mode
+    f = open('./upload/' + fileName, "wb")
+    
+    while True:
+        # read 4096 bytes from the socket (file data)
+        dataRead, address = s.recvfrom(4096)
+        if dataRead.decode() == 'FILE END':    
+            # file transmission is done
+            print('File transmission is done');
+            break
+        # write to the file the bytes we just received
+        f.write(dataRead)
+        # update the progress bar
+        print('write %d bytes into file' % len(dataRead))
+        
+    f.close()
+
 while(True):
+    
+    #get command from client
     data, address = s.recvfrom(4096)
     print('received message from %s %s' % (data, address))
-    # gestisce le alternative del menu e restituisce il messaggio di riferimento
-
+    
     command = data.decode()
-
-    if(command == 'list'):
+    
+    #command switch for list, get, put
+    
+    if command == 'list':
+        #send to client files list as json response
         files = json.dumps(os.listdir('./upload'))
         s.sendto(files.encode(), address)
-    elif(data == 'get'):
-        break
-    elif(data == 'put'):
+    elif command == 'get':
+        pass
+    elif command == 'put':
+        #send to client ready message to receive filename to upload
+        print('Read Put')
+        s.sendto('PUT ACK'.encode(), address)
+        
+        #receive filename to upload
+        fileName, address = s.recvfrom(4096)
+        print('Received filename')
+        
+        #send to client filename ack to receive file data
+        s.sendto('FILENAME ACK'.encode(), address)
+        print('Starting get Data')
+        
+        #call function to handle file data flow
+        saveFile(fileName.decode(), s)
+        
         break
     else:
         break
