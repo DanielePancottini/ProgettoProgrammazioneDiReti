80500ffee22ef2ef0219bb6f0efb5f92659033d4
status <marco.costantini7@studio.unibo.it>
2022-07-22 13:03:17 +0200

    First implementation of menu in client

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 
 import socket
-import json
 import os
 
 import rdt_handler
@@ -10,36 +9,78 @@ putHandler = rdt_handler.RdtFileTransferHandler();
 
 serverAddress = ('localhost', 10000)
 
+initialMessage="ready"
+uploadPath = os.getcwd()+"\\Upload\\"
+
 payload = {
     "command": "put",
     "fileName": "grandeFile.txt"
 }
 
+choice=-1
+def menu():
+    print("-------------------------")
+    print("Welcome\n")
+    print("1. File List")
+    print("2. Put File")
+    print("3. Get File")
+    print("4. Exit")
+    print("------------------------")    
+    
+ 
 try:
+    #send ready message to server
+    socketUDP.sendto(initialMessage.encode(), serverAddress)
     
-    if(not os.path.exists(payload['fileName'])):
-        print('File not exists')
-        socketUDP.close()
+    #wait server's answer
+    ready, address = socketUDP.recvfrom(4096)
     
-    print('sending put command')
-    socketUDP.sendto(json.dumps(payload).encode(), serverAddress)
-    
-    #wait for command ack
-    
-    socketUDP.settimeout(0.5)
-    
-    try:    
+    while(True):
+        menu()
+        choice=input("Choose[1..4]: ")
+        #send choice to server
+        socketUDP.sendto(choice.encode(), serverAddress)
+        #wait server's answer
         ready, address = socketUDP.recvfrom(4096)
-    except socket.timeout:
-        print("Error during command transmission, closing socket")   
-        socketUDP.close()
+        
+        if(choice == 1):
+            #Print file list
+            files = os.listdir(uploadPath)
+            print(files)
+        elif(choice == 2):
+            print("Operazione --alfa--")
+        elif(choice == 3):
+            print("Operazione --beta--")
+        elif(choice == 4): 
+            print("exit...")
+            break
+        else:
+            print("Wrong choice")
     
-    socketUDP.settimeout(None)
+    # if(not os.path.exists(payload['fileName'])):
+    #     print('File not exists')
+    #     socketUDP.close()
     
-    putHandler.rdtFileDataSender(payload['fileName'], socketUDP, serverAddress)
+    # print('sending put command')
+    # socketUDP.sendto(json.dumps(payload).encode(), serverAddress)
+    
+    # #wait for command ack
+    
+    # socketUDP.settimeout(0.5)
+    
+    # try:    
+    #     ready, address = socketUDP.recvfrom(4096)
+    # except socket.timeout:
+    #     print("Error during command transmission, closing socket")   
+    #     socketUDP.close()
+    
+    # socketUDP.settimeout(None)
+    
+    # putHandler.rdtFileDataSender(payload['fileName'], socketUDP, serverAddress)
 
 except Exception as info:
     print(info)
 finally: 
     print('closing socket')
     socketUDP.close()
+    
