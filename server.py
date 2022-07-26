a9c1f04acfa27ae4dc43277c6d3638349c396b4c
Daniele Pancottini <43572647+DanielePancottini@users.noreply.github.com>
2022-07-26 10:54:15 +0200

    Fixed checksum control into rdt protocol module; Done some comments into server module

--- server.py
+++ server.py
@@ -4,9 +4,15 @@ import os
 
 import rdt_handler
 
+# udp server socket definition and binding
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', 10000))
 
+#welcome message
+print('Server Running...')
+
+# declaring RdtFileTransferHandler object to handle rdt file transfer transmission
+# between client and server
 rdtHandler = rdt_handler.RdtFileTransferHandler()
 filesPath = './upload'
 
@@ -14,22 +20,22 @@ try:
         
     while(True):
         
-        #get command from client
+        # get command from client (list, put, get)
         rawCommandPacket, address = s.recvfrom(4096)
         print('Received Command: %s | From: %s' % (rawCommandPacket, address))
         
-        #split command by other params
+        # split command by other params
         commandPacket = rawCommandPacket.decode().split()
         command = commandPacket[0]
         
         #command switch for list, get, put
         if command.lower() == 'list':
-            #send to client files list as array of strings
+            # send to client files list as array of strings
             files = str(os.listdir(filesPath))
             s.sendto(files.encode(), address)
             
         elif command.lower() == 'get':
-            #check if file exists, if not send error, otherwise start rdt file transmission for get
+            # check if file exists, if not send error, otherwise start rdt file transmission for get
             filename = commandPacket[1]
             if(os.path.exists(filesPath + '/' + filename) == False):
                 s.sendto('ERROR FILENAME'.encode(), address)
@@ -42,8 +48,8 @@ try:
             #send command ack and start rdt file transmission for put
             filename = commandPacket[1]
             s.sendto('PUT ACK'.encode(), address)
-            
             rdtHandler.rdtFileDataReceiver(filesPath + '/' + filename, s)
 
 except Exception as error:
     print(error)
+    s.close()
